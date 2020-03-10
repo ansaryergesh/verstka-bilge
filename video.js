7c51031f3b1560eaffede770f4ac926d323887b7
ansaryergesh <yergeshovansar@gmail.com>
2020-03-10 15:46:08 +0600

    fix

--- video.js
+++ video.js
@@ -1,35 +1,37 @@
 
-// let video = document.querySelectorAll('video');
+let video = document.querySelectorAll('video');
 
-// function isVisible(elem) {
+function isVisible(elem) {
 
-//   let coords = elem.getBoundingClientRect();
+  let coords = elem.getBoundingClientRect();
 
-//   let windowHeight = document.documentElement.clientHeight;
+  let windowHeight = document.documentElement.clientHeight;
 
-//   // верхний край элемента виден?
-//   let topVisible = coords.top - 100 > 0 && coords.top + 400 < windowHeight;
+  // верхний край элемента виден?
+  let topVisible = coords.top > 0 && coords.top + 400 < windowHeight;
 
-//   // нижний край элемента виден?
-//   let bottomVisible = coords.bottom - 100 < windowHeight && coords.bottom > 200;
+  // нижний край элемента виден?
+  let bottomVisible = coords.bottom - 100 < windowHeight && coords.bottom > 400;
 
-//   return topVisible || bottomVisible;
-// }
+  return topVisible || bottomVisible;
+}
 
-// function showVisible() {
-//   for (let video of document.querySelectorAll('video')) {
-//     if (isVisible(video)) {
-//       video.autoplay = true;
-//       video.play();
-//       video.loop = true;
-//       window.onscroll = showVisible;
-//     } else {
-//       video.load();
-//     //   window.onscroll = '';
+function showVisible() {
+  for (let video of document.querySelectorAll('video')) {
+    if (isVisible(video)) {
+      video.autoplay = true;
+      video.play();
+      video.loop = true;
+      window.onscroll = showVisible;
+    } else {
+    //   video.load();
 
-//     }
-//   }
-// }
+    }
+  }
+}
 
-// showVisible();
-// window.onscroll = showVisible;
+showVisible();
+window.addEventListener('scroll', (e) => {
+    e.preventDefault();
+    showVisible();
+})
